62861a2144f5507edefdcda67d6d1d0a133e27ae
Ken Bingham <ken.bingham64@gmail.com>
2024-03-05 05:59:16 -0700

    Initial commit

---
+++ src/main/java/com/acmeairlines/airlinesim/AirlinesimApplication.java
@@ -0,0 +1,13 @@
+package com.acmeairlines.airlinesim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlinesimApplication.class, args);
+    }
+
+}
